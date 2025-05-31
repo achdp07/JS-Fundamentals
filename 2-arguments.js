00304005403afded5e1a0960a091ae9dbb9700d7
achdp07 <layefils26@gmail.com>
2025-05-31 20:34:20 +0200

    Update

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-const myVAr = 0;
+const args = process.argv.slice(2);
 
-if (myVAr == 0) {
+if (args.length === 0) {
   console.log("No argument");
-} else if (myVAr == 1) {
+} else if (args.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
