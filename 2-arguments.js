26531869554e70e69182dac0e230538474fef66f
achdp07 <layefils26@gmail.com>
2025-05-31 20:31:56 +0200

    Update

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ let myVAr = 0;
 
 if (myVAr == 0) {
   console.log("No argument");
-} else if (myVAr == 1) {
+} else if (myVAr != 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
