e5f3760767bfad1ffc4342a21d86114112eadb54
achdp07 <layefils26@gmail.com>
2025-05-31 21:22:29 +0200

    Update

--- 9-add.js
+++ 9-add.js
@@ -1,11 +1,8 @@
-const x = parseInt(process.argv[2]);
-
-if (isNaN(x)) {
-  console.log("Missing number of occurrences");
-} else {
-  let i = 0;
-  while (i < x) {
-    console.log("C is fun");
-    i++;
-  }
+function add(a, b) {
+  return a + b;
 }
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+console.log(add(a, b));
